6f359d86f915e0b8a3473665bb27f2f6f2cad19d
DorMiX <dormany.robert@gmail.com>
2020-02-07 20:26:49 +0100

    new topic form is generated

--- web_board/web_board/urls.py
+++ web_board/web_board/urls.py
@@ -20,6 +20,7 @@ from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('board/<int:pk>', views.board_topics, name='board_topics'),
+    path('board/<int:pk>/', views.board_topics, name='board_topics'),
+    path('board/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
