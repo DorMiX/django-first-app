3fad6dc7bb88a3259b76cbf53dfe3f0b9905aabb
DorMiX <dormany.robert@gmail.com>
2020-02-07 16:35:07 +0100

    Urls tests work

--- web_board/boards/views.py
+++ web_board/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 # Create your views here.
 # from django.http import HttpResponse
@@ -6,19 +6,8 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    # boards_names = list()
-    #
-    # for board in boards:
-    #     boards_names.append(board.name)
-    #
-    # response_html = '<br>'.join(boards_names)
-    #
-    # return HttpResponse(response_html)
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
-    try:
-        board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
