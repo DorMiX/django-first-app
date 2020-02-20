44960be63aec66617f37710ed42070c587110f7b
DorMiX <dormany.robert@gmail.com>
2020-02-20 15:20:49 +0100

    Sign Up - Testing the Sign Up View

--- web_board/web_board/urls.py
+++ web_board/web_board/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from boards import views
+from accounts import views as accounts_view
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', accounts_view.signup, name='signup'),
     path('board/<int:pk>/', views.board_topics, name='board_topics'),
     path('board/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
